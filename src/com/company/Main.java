e750090d0aba4e44534d9e2d68c544f1f4156de1
hamzatz <hamzatzaitov@gmail.com>
2019-03-24 21:46:12 +0600

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    final String text1 = "Привет,";
+    String text2 = "мир !";
+    String text = text1+text2;
+        System.out.println(text);
+        System.out.println(text1);
+    }
+}
