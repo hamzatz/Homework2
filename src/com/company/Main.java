aa85adfd5a3f3779435fa186dd21129b620c6e54
hamzatz <hamzatzaitov@gmail.com>
2019-03-24 21:57:01 +0600

    my first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
     final String text1 = "Привет,";
-    String text2 = "мир !";
+    String text2 = "мир ! Ты так прекрасен!";
     String text = text1+text2;
         System.out.println(text);
         System.out.println(text1);
