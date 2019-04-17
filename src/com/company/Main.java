81c564dac392b3cce839022d03d4c308981fc5cf
hamzatz <hamzatzaitov@gmail.com>
2019-04-17 17:54:23 +0600

    new project with if else

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Хамзат";
-        int age = 45;
-        int grads = 25;
-        {
-            if (grads < 30 || grads > -20) {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Как тебя зовут?");
+            String name = scanner.nextLine();
+            System.out.println("Сколько тебе лет?");
+            int age = scanner.nextInt();
+            System.out.println("Сколько градусов на улице?");
+            int grads = scanner.nextInt();
+
+            if (grads >= 30 || grads < -20 && age > 20 && age < 45) {
                 System.out.println(name + " тебе нельзя на улицу!");
-                {
-                    if (age == 20 || age > 40) ;
 
-                    else {
-                        System.out.println("Ты идешь к своему другу!");
-                    }
-                    {
-                        if (age > 20) ;
+            } else if ( grads <= 0 || grads > 28 &&age < 20 ) {
+                System.out.println(name + " тебе нельзя на улицу!");
 
-                        else if (grads > 0 || grads < 28);
-
-                        else {
-                            System.out.println("Ты идешь к своему другу!");
-                        }
-                        {
-                            if (age < 45) ;
-                            else if (grads < -10 || grads > 25);
-                            }
-                        }
-                    }
-                }
+            } else if ( grads < -10|| grads > 25 && age > 45 ) {
+                System.out.println(name + " тебе нельзя на улицу!");
+            } else {
+                System.out.println("Ты идешь к своему другу!");
             }
         }
     }
+}
